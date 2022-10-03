30010f6e6a0308efdd9344d33f4d6291693ef1f2
mss38 <mss38k@gmail.com>
2022-10-04 00:29:16 +0600

    setup json server initially

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const auth = require("json-server-auth");
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 9000;
+
+// /!\ Bind the router db to the app
+server.db = router.db;
+
+server.use(middlewares);
+
+const rules = auth.rewriter({
+  // Permission rules
+  users: 640,
+  conversations: 660,
+  messages: 660,
+});
+
+server.use(rules);
+server.use(auth);
+server.use(router);
+
+server.listen(port);
