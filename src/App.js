b8bc6aceb0560ae7b65f858341edf7019e8d8110
mss38 <mss38k@gmail.com>
2022-10-05 00:23:26 +0600

    added ant design ui component library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { DownloadOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import React from "react";
+import "./App.css";
+
 function App() {
   return (
     <div>
       <h2>Hello World</h2>
+      <Button className="font-semibold text-red-700" type="primary" shape="round" icon={<DownloadOutlined />}>
+        Button Antd
+      </Button>
     </div>
   );
 }
